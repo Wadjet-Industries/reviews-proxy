da70db4aded0fa2aeea44600d09d4b6766d644af
Jessica Yu <jessyu2010@gmail.com>
2019-09-27 15:10:48 -0700

    Added server response for requests for Loaderio file

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -9,6 +9,13 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/listing/:id/', express.static('public'));
+app.use('/loaderio-092c547091c949f3c5e53b222b1d7ee0/', (req, res) => {
+  res.sendFile('/public/loaderio-092c547091c949f3c5e53b222b1d7ee0.txt', (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('GET seen');
